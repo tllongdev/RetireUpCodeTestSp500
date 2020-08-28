2c7b386b1f57d7d666ef44a04e563880397a6e80
Timothy Lee Long <tllongdev@gmail.com>
2020-08-28 17:15:09 -0500

    font and styling updates

--- sp500/components/NavBar.js
+++ sp500/components/NavBar.js
@@ -2,8 +2,10 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-boots
 
 export default function NavBar() {
 	return (
-		<Navbar bg='primary' expand='md' variant='dark'>
-			<Navbar.Brand className='font-italic'>Slickcharts</Navbar.Brand>
+		<Navbar bg='primary' expand='md' variant='dark' style={{ background: 'linear-gradient(#ffffff15, #ffffff25)' }}>
+			<Navbar.Brand className='font-italic' style={{ fontWeight: 300, fontSize: 17.75 }}>
+				Slickcharts
+			</Navbar.Brand>
 			<Navbar.Toggle aria-controls='basic-navbar-nav' />
 			<Navbar.Collapse id='basic-navbar-nav'></Navbar.Collapse>
 		</Navbar>
--- sp500/pages/_app.js
+++ sp500/pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'rc-slider/assets/index.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
